6e33500922ee0c1c3fa2f9b6d39d74161c0b893a
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-01-30 22:42:49 -0500

    added 404 page and restructured routes folder

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+  
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const htmlRoutes = require("./html");
+
+
+router.use("/", htmlRoutes);
+router.use("/api", apiRoutes);
+
+
+router.get("*", function(req, res) {
+  res.send("<h1>404 error</h1>");
+});
+
+// why won't this work?
+// router.get("*", function(req, res) {
+  
+//   res.sendFile(path.join(__dirname, "../public/notes.html"));
+  
+//   });
+
+
+module.exports = router;
