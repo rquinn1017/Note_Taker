c9840c0cb380ba3fe7c56917c9a2a9a8f750bf8a
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-01-31 02:52:36 -0500

    updated jawsdb

--- db/connection.js
+++ db/connection.js
@@ -1,26 +1,18 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-
-// var db_config = {
-//   host: 'us-cdbr-iron-east-04.cleardb.net',
-//   user: 'b5b2fb599475ec',
-//   password: '53ac9b01',
-//   database: 'heroku_f53a33e5c1c71bb'
-// };
 var connection;
 // Sets up db to connect locally or on JAWSDB if deployed
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-   connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: process.env.MYSQL_PW,
     database: "notetaker_db"
   });
 }
-
 
 // Turns BOOLEAN 0s and 1s returned from the db into true and false
 connection.config.typeCast = function(field, next) {
