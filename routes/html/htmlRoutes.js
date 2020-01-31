22a6c8b88e70f5acda3f6f6640650f02acf5820a
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-01-31 00:29:07 -0500

    updated dirname

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -4,11 +4,11 @@ module.exports = function(app){
     
     // Route to go to notes page
     app.get("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes"));
     });
 
     // Route to go to index page
     app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index"));
     });
 }
